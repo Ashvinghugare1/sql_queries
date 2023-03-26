b1b42c2969521a06b9891242c09f46c8bed0634b
Ashvinghugare1 <ashvinghugare1@gmail.com>
2023-03-26 13:00:03 +0530

    initial commit

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        factory(App/User::class, 3)->create();
+    }
+}
